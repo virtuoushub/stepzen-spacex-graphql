249c5456715d45cab9dbce427fb94b68c2fc5f20
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2021-10-11 17:33:01 -0400

    chore: run formatter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import { gql, useQuery } from '@apollo/client';
-import StepZenLogo from './light-blue.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Rockets from './rocket-information';
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import StepZenLogo from "./light-blue.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Rockets from "./rocket-information";
 
 export const GET_QUERY = gql`
   query MyQuery   {
@@ -40,24 +40,22 @@ function Home() {
 
   if (loading) return <p>Loading ...</p>;
 
-  if (error) return (
-    <pre>{JSON.stringify(error, null, 2)}</pre>
-  );
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
   return (
       <div className="App">
         <header className="App-header">
           <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
-          <p style={{ marginTop: "40px" }}>Capsule information pulled from StepZen Endpoint:</p>
+        <p style={{ marginTop: "40px" }}>
+          Capsule information pulled from StepZen Endpoint:
+        </p>
             <ul>
               <li>id: {data.capsule.id}</li>
               <li>landings: {data.capsule.landings}</li>
               <li>reuse-count: {data.capsule.reuse_count}</li>
             </ul>
-          <Link to='/rockets'>Rockets Information</Link>
+        <Link to="/rockets">Rockets Information</Link>
         </header>
       </div>
   );
-
 }
-;
 export default App;
--- src/rocket-information/index.js
+++ src/rocket-information/index.js
@@ -1,6 +1,6 @@
-import './index.css';
-import { gql, useQuery } from '@apollo/client';
-import StepZenLogo from '../light-blue.svg';
+import "./index.css";
+import { gql, useQuery } from "@apollo/client";
+import StepZenLogo from "../light-blue.svg";
 const GET_QUERY = gql`
   query MyQuery   {
     rockets(limit: 5) {
@@ -24,20 +24,22 @@ function Rockets() {
 
     if (loading) return <p>Loading ...</p>;
 
-    if (error) return (
-        <pre>{JSON.stringify(error, null, 2)}</pre>
-    );
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
     const rockets = data.rockets;
-    const listitems = rockets.map((rocket) => <>
-        <li>Id: {rocket.id}, Name: {rocket.name}, Country: {rocket.country}, Cost: {rocket.cost_per_launch}</li></>);
+  const listitems = rockets.map((rocket) => (
+    <>
+      <li>
+        Id: {rocket.id}, Name: {rocket.name}, Country: {rocket.country}, Cost:{" "}
+        {rocket.cost_per_launch}
+      </li>
+    </>
+  ));
     return (
         <div className="App">
             <header className="App-header">
                 <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
                 <p style={{ marginTop: "40px" }}>Top 5 Rockets information:</p>
-                <ul>
-                    {listitems}
-                </ul>
+        <ul>{listitems}</ul>
             </header>
         </div>
     );
