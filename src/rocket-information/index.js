21a4f7c76268bf49a27d7ec92c4d6215314cbe48
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2021-10-11 17:35:32 -0400

    chore: camelCase variable

--- src/rocket-information/index.js
+++ src/rocket-information/index.js
@@ -2,44 +2,44 @@ import "./index.css";
 import { gql, useQuery } from "@apollo/client";
 import StepZenLogo from "../light-blue.svg";
 const GET_QUERY = gql`
-  query MyQuery   {
+  query MyQuery {
     rockets(limit: 5) {
-        id
-        name
-        mass {
-          kg
-          lb
-        }
-        company
-        cost_per_launch
-        country
-        description
+      id
+      name
+      mass {
+        kg
+        lb
       }
+      company
+      cost_per_launch
+      country
+      description
+    }
   }
 `;
 
 function Rockets() {
-    const { loading, error, data } = useQuery(GET_QUERY);
+  const { loading, error, data } = useQuery(GET_QUERY);
 
-    if (loading) return <p>Loading ...</p>;
+  if (loading) return <p>Loading ...</p>;
 
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    const rockets = data.rockets;
-  const listitems = rockets.map((rocket) => (
-      <li>
-        Id: {rocket.id}, Name: {rocket.name}, Country: {rocket.country}, Cost:{" "}
-        {rocket.cost_per_launch}
-      </li>
+  const rockets = data.rockets;
+  const listItems = rockets.map((rocket) => (
+    <li>
+      Id: {rocket.id}, Name: {rocket.name}, Country: {rocket.country}, Cost:{" "}
+      {rocket.cost_per_launch}
+    </li>
   ));
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
-                <p style={{ marginTop: "40px" }}>Top 5 Rockets information:</p>
-        <ul>{listitems}</ul>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
+        <p style={{ marginTop: "40px" }}>Top 5 Rockets information:</p>
+        <ul>{listItems}</ul>
+      </header>
+    </div>
+  );
 }
 
 export default Rockets;
